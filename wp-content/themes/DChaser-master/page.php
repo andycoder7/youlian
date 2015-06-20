db6c13ab9581570a29cdf1295012a560e7e0841d
yaoshanliang <1329517386@qq.com>
2015-06-20 22:57:38 +0800

    修改page，根据url决定进入价格计算

--- wp-content/themes/DChaser-master/page.php
+++ wp-content/themes/DChaser-master/page.php
@@ -1,6 +1,20 @@
-<?php 
+<?php
+	//根据url进入到配置方案和价格计算
+	$url = $_SERVER['REQUEST_URI'];
+	if(preg_match('/configuration/', $url))
+	{
+		include_once('configuration.php');
+		exit;
+	}
+	if(preg_match('/calculate_price/', $url))
+	{
+		include_once('calculate_price.php');
+		exit;
+	}
+?>
+<?php
  get_header('meta');
- get_header(); 
+ get_header();
  ?>
 <div class="heading-top">
 当前位置：<a href="<?php bloginfo('siteurl'); ?>/" title="返回首页">首页</a> > <?php the_title(); ?>
@@ -30,4 +44,4 @@
         <!-- /Main Content -->
 <?php get_sidebar(); ?></div>
 
-<?php get_footer(); ?> 
+<?php get_footer(); ?>
