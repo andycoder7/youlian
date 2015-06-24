48d3c0f20d26e49d86a2f343a1dd00ce19db4b90
yaoshanliang <1329517386@qq.com>
2015-06-24 20:00:58 +0800

    添加ucenter插件

---
+++ wp-content/plugins/ucenter-integration/config.php
@@ -0,0 +1,16 @@
+<?php
+define('UC_CONNECT', 'mysql');
+define('UC_DBHOST', 'localhost');
+define('UC_DBUSER', 'root');
+define('UC_DBPW', 'iat');
+define('UC_DBNAME', 'ucenter');
+define('UC_DBCHARSET', 'utf8');
+define('UC_DBTABLEPRE', '`ucenter`.uc_');
+define('UC_DBCONNECT', '0');
+define('UC_KEY', '8739Jjo6kOv6Tfxf71kY/91IDEgIkwRYjtGYVzM');
+define('UC_API', 'http://localhost/ucenter');
+define('UC_CHARSET', 'utf8');
+define('UC_IP', '');
+define('UC_APPID', '1');
+define('UC_PPP', '20');
+?>
