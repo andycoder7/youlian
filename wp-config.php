04c88db6c25766cd17287027d63d79bd84ab6c5c
Andy.Yao <andy.at.working@gmail.com>
2015-07-23 21:42:36 +0800

    modify .htaccess and config to adapt to andy's pc

--- wp-config.php
+++ wp-config.php
@@ -15,13 +15,13 @@
 
 // ** MySQL 设置 - 具体信息来自您正在使用的主机 ** //
 /** WordPress数据库的名称 */
-define('DB_NAME', 'wordpress');
+define('DB_NAME', 'youlian');
 
 /** MySQL数据库用户名 */
 define('DB_USER', 'root');
 
 /** MySQL数据库密码 */
-define('DB_PASSWORD', 'iat');
+define('DB_PASSWORD', 'sklcc341_A');
 
 /** MySQL主机 */
 define('DB_HOST', 'localhost');
