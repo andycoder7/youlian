9aa9258d11a55e36893abcddb8539587ec1b3295
yaoshanliang <1329517386@qq.com>
2015-07-30 13:24:00 +0800

    插件启用与停用

--- wp-content/plugins/youlian/class.youlian.php
+++ wp-content/plugins/youlian/class.youlian.php
@@ -1,2 +1,29 @@
 <?php
-class Youlian
+class Youlian {
+
+	//启用插件
+	public static function plugin_activation() {
+	}
+
+	//停用插件
+	public static function plugin_deactivation() {
+	}
+
+	//初始化
+	public static function init() {
+		if( is_admin() ) {
+			/*  利用 admin_menu 钩子，添加菜单 */
+			add_action('admin_menu', 'display_copyright_menu');
+		}
+
+		function display_copyright_menu() {
+			/* add_options_page( $page_title, $menu_title, $capability, $menu_slug, $function);  */
+			/* 页名称，菜单名称，访问级别，菜单别名，点击该菜单时的回调函数（用以显示设置页面） */
+			add_options_page('Set Copyright', '参数配置', 'administrator','youlian_config', 'display_copyright_html_page');
+		}
+		function display_copyright_html_page() {
+		echo '111';
+		exit;
+		}
+	}
+}
--- wp-content/plugins/youlian/youlian.php
+++ wp-content/plugins/youlian/youlian.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: 友联网站参数配置插件
 Plugin URI: http://iat.net.cn
-Description: 配置参数
+Description: 友联网站专用插件，用于配置参数
 Version: 0.1
 Author: iat
 Author URI: http://iat.net.cn
@@ -17,10 +17,11 @@ define( 'YOULIAN_VERSION', '0.1' );
 define( 'YOULIAN_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'YOULIAN_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 
+require_once( YOULIAN_PLUGIN_DIR . 'class.youlian.php' );
+require_once( YOULIAN_PLUGIN_DIR . 'class.youlian_admin.php' );
+
 register_activation_hook( __FILE__, array( 'youlian', 'plugin_activation' ) );
 register_deactivation_hook( __FILE__, array( 'youlian', 'plugin_deactivation' ) );
 
-require_once( YOULIAN_PLUGIN_DIR . 'class.youlian.php' );
-require_once( YOULIAN__PLUGIN_DIR . 'class.youlian-widget.php' );
 
 add_action( 'init', array( 'youlian', 'init' ) );
