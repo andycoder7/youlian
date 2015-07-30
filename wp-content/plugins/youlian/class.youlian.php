a05a6ac21f40133a917fbc39896a49a6d318af60
yaoshanliang <1329517386@qq.com>
2015-07-30 14:57:02 +0800

    update

--- wp-content/plugins/youlian/class.youlian.php
+++ wp-content/plugins/youlian/class.youlian.php
@@ -3,27 +3,53 @@ class Youlian {
 
 	//启用插件
 	public static function plugin_activation() {
+		add_option("display_copyright_text", "<p style='color:red'>本站点所有文章均为原创，转载请注明出处！</p>p>", '', 'yes');
 	}
 
 	//停用插件
 	public static function plugin_deactivation() {
+		 // delete_option('display_copyright_text');
 	}
 
 	//初始化
 	public static function init() {
 		if( is_admin() ) {
-			/*  利用 admin_menu 钩子，添加菜单 */
-			add_action('admin_menu', 'display_copyright_menu');
+			//利用 admin_menu 钩子，添加菜单
+			add_action('admin_menu', array('youlian', 'display_config_menu'));
 		}
 
-		function display_copyright_menu() {
-			/* add_options_page( $page_title, $menu_title, $capability, $menu_slug, $function);  */
-			/* 页名称，菜单名称，访问级别，菜单别名，点击该菜单时的回调函数（用以显示设置页面） */
-			add_options_page('Set Copyright', '参数配置', 'administrator','youlian_config', 'display_copyright_html_page');
-		}
-		function display_copyright_html_page() {
-		echo '111';
-		exit;
-		}
 	}
+
+	//显示配置菜单
+	public static function display_config_menu() {
+		//add_options_page( $page_title, $menu_title, $capability, $menu_slug, $function);
+		//页名称，菜单名称，访问级别，菜单别名，点击该菜单时的回调函数（用以显示设置页面）
+		add_options_page('Set Config', '参数配置', 'administrator','config', array('youlian', 'display_config_html_page'));
+	}
+
+	//显示配置页面
+	public static function display_config_html_page() {
+?>
+    <div>
+        <h2>文章目录设置</h2>
+        <form method="post">
+            <?php /* 下面这行代码用来保存表单中内容到数据库 */
+            if ( 'save' == $_REQUEST['action'] )
+            update_option( ci_fontsize, $_REQUEST['ci_fontsize'] );
+            ?>
+            <p>
+                <textarea>
+                    name="ci_fontsize"
+                    id="ci_fontsize"
+                    cols="40"
+                    rows="6"><?php echo get_option('ci_fontsize'); ?></textarea>
+            </p>
+
+            <p>
+                <input type="submit" name="action" value="save" class="button-primary" />
+            </p>
+        </form>
+    </div>
+<?php
+}
 }
